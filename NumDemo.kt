578237b7d97108a63eadaa00d7d379c5b1a83c85
Ambat Vipin <ambat.vipu@gmail.com>
2022-06-17 06:17:32 +0530

    Maths operation on Variable

---
+++ NumDemo.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>)
+
+{
+   // program to perform mathe mathematical operation using Kotlin
+
+    val a=10
+
+    val b=20
+
+    println(a+b)
+    println(b-a)
+    println(b*a)
+    println(b/a)
+    print(b%a) //modulo div
+
+
+
+
+}
